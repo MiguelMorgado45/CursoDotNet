cc9e1d8f734e35db2f08785de47eb7c49616eb00
Miguel Angel Morgado Huerta <miguel.morgado@timeweb.mx>
2023-03-07 09:05:08 -0600

    Agrega ejercicio B: Área y Perímetro

---
+++ PerimetroArea/PerimetroArea/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Programa de perímetro y área - Miguel Angel Morgado Huerta - Ejercicio B");
+
+var l = 7;
+
+// Perímetro
+var hipotenusa = Math.Sqrt(Math.Pow(l, 2) + Math.Pow(3 * l, 2));
+var perimetroCirculo = (Math.PI * (3 * l)) / 2;
+var perimetroRectangulo = (4 * l) + (3 * l);
+var perimetro = (2 * hipotenusa) + perimetroRectangulo + perimetroCirculo;
+
+Console.WriteLine($"Perímetro: {perimetro}");
+
+// Área
+var areaCirculo = (Math.PI * Math.Pow(1.5 * l, 2)) / 2;
+var areaRectangulo = (3 * l) * l;
+var areaTriangulo = ((3 * l) * l) / 2;
+
+var area = areaCirculo + (2 * areaRectangulo) + (2 * areaTriangulo);
+
+Console.WriteLine($"Área: {area}");
